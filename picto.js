198c2aaf2c20e9caca1c689c1a0466a67653ee7f
Edward <eddy.hey@outlook.com>
2018-05-15 13:32:39 +0100

    Add temporary validation of room codes and jQuery. (TODO: Remove jQuery)

--- picto.js
+++ picto.js
@@ -2,14 +2,30 @@ var express = require('express')
 var app = express()
 var path = require('path')
 
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+
+
 var WebSocketServer = require('ws').Server
 var wss = new WebSocketServer({port: 40510})
 
 app.use(express.static('public'))
 
-app.get('/api/', function (req, res) {
-  return
+app.get('/room/:roomcode', function (req, res) {
+  return res.send(req.params)
 })
+
+app.get('/api/', function (req, res) {
+  console.log(req.query)
+  if (req.query.roomCode == 'abc') {
+    var avail = true
+  } else {
+    var avail = false
+  }
+  return res.send({roomCode: req.query.roomCode, available: avail})
+})
+
 
 app.listen(8000, function () {
   console.log('Example app listening on port 8000!')
