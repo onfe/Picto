226770c8f2c4de087a521ef3ada2ba40713a0dc9
Edward H <eddy.hey@outlook.com>
2018-06-07 12:37:39 +0100

    Class-ify it

--- req/token.js
+++ req/token.js
@@ -5,29 +5,28 @@ var jwt = require('jsonwebtoken');
 const uuidV4 = require('uuid/v4');
 const secret = uuidV4();
 
-function create(n, r) {
-  return jwt.sign({ name: n, room: r }, secret, {
-      expiresIn: 86400 // expires in 24 hours
-    })
-}
+module.exports = class Token {
+  static create(n, r) {
+    return jwt.sign({ name: n, room: r }, secret, {
+        expiresIn: 86400 // expires in 24 hours
+      })
+  }
 
-function verify(t, n, r) {
-  var result = jwt.verify(t, secret, function(err, decoded) {
-    if (err) {
-      console.log(err)
+  static verify(t, n, r) {
+    var result = jwt.verify(t, secret, function(err, decoded) {
+      if (err) {
+        console.log(err)
+        return false;
+      }
+
+      if (decoded.name === n && decoded.room === r) {
+        // the token matches the user name, neat.
+        return true;
+      }
+
       return false;
-    }
 
-    if (decoded.name === n && decoded.room === r) {
-      // the token matches the user name, neat.
-      return true;
-    }
-
-    return false;
-
-  });
-  return result;
+    });
+    return result;
+  }
 }
-
-module.exports.create = create;
-module.exports.verify = verify;
