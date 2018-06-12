0796ebc3afc67da047977f449178c68bff0fd9dc
Edward <eddy.hey@outlook.com>
2018-06-12 23:14:45 +0100

    Initial commit.

---
+++ req/utils.js
@@ -0,0 +1,27 @@
+"use strict"; // Force ES6 and strict mode.
+
+module.exports.shuffle = function (array) {
+    let counter = array.length;
+
+    // While there are elements in the array
+    while (counter > 0) {
+        // Pick a random index
+        let index = Math.floor(Math.random() * counter);
+
+        // Decrease counter by 1
+        counter--;
+
+        // And swap the last element with it
+        let temp = array[counter];
+        array[counter] = array[index];
+        array[index] = temp;
+    }
+
+    return array;
+}
+
+module.exports.getRandomInt = function (min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
