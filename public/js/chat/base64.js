d04926a624e4612ecfb0d66a7083a626a2427260
Edward <eddy.hey@outlook.com>
2018-07-10 15:59:06 +0100

    Add Base64 helper module.

---
+++ public/js/chat/base64.js
@@ -0,0 +1,30 @@
+module.exports = class Base64 {
+
+  static decode(st) {
+    var out = '';
+    var base64List = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-=';
+
+    for (var i = 0; i < st.length; i++) {
+      var char = st[i];
+      var num = base64List.indexOf(char);
+      var bin = num.toString(2);
+      var final = ('000000' + bin).slice(-6); // ensure is 6 chars
+      out += final;
+    }
+
+    return out;
+  }
+
+  static encode(st) {
+    var out = '';
+    var base64List = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-=';
+    var chunks = chunkString(st, 6);
+    for (var c = 0; c < chunks.length; c++) {
+      var num = parseInt(chunks[c], 2) // Binary string to decimal integer.
+      var char = base64List[num];
+      out += char.toString(2);
+
+    }
+    return out;
+  }
+}
