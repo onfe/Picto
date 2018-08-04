c4af4e5aef991a98252961df7d16fd6f4895d7d5
Edward <eddy.hey@outlook.com>
2018-08-04 16:37:24 +0100

    Add smooth scrolling to incoming messages.

--- public/js/chat/messagehistory.js
+++ public/js/chat/messagehistory.js
@@ -12,29 +12,44 @@ module.exports = class MessageHistory {
   addMessage(id, sender, colour, content) {
     // Recieve a b64 encoded message, add it to the message history and draw it.
     let msg = new Message(id, content, sender, colour);
-    console.log(msg)
-    this.messages.push(msg);
-    $("#msg-hist").prepend(msg.html);
-    msg.draw();
+    this.add(msg)
 
-    this.trim();
-
+    msg.draw(); // draw the message
   }
 
   addStatus(id, content) {
     let stat = new Status(id, content);
-    this.messages.push(stat);
-    $("#msg-hist").prepend(stat.html);
-
-    this.trim();
+    this.add(stat)
   }
 
   addSysMsg(id, content) {
     let sys = new SysMessage(id, content);
-    this.messages.push(sys);
-    $("#msg-hist").prepend(sys.html);
+    this.add(sys)
+  }
 
-    this.trim();
+  add(msg) {
+    this.messages.push(msg)
+    this.trim()
+
+    var willScroll = false;
+    var scrollBefore = $('#msg-hist').scrollTop()
+    var histHeight = $('#msg-hist').height()
+    var scrollHeight = $("#msg-hist")[0].scrollHeight;
+    if ((scrollHeight - (scrollBefore + histHeight)) <= 10) { // if the message history is not focused on the most recent messages (+ tolerance), do not scroll.
+      console.log(scrollHeight, scrollBefore, scrollHeight)
+      var willScroll = true;
+    }
+    console.log(willScroll);
+
+    $("#msg-hist").prepend(msg.html); // add message html
+
+    var newScroll = $("#msg-hist")[0].scrollHeight - $('#msg-hist').height();
+
+    $('#msg-hist').scrollTop(scrollBefore)
+    if (willScroll) {
+      $('#msg-hist').animate({scrollTop: newScroll}, 300);
+    }
+
   }
 
   trim() {
