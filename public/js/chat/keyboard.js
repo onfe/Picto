d58a3b2e1dad2cc723f0a1fb5583f958084c2bc5
Edward <eddy.hey@outlook.com>
2018-09-01 17:29:59 +0100

    Writing this JSON sucks, if only there was a web-app for that...

---
+++ public/js/chat/keyboard.js
@@ -0,0 +1,26 @@
+module.export = class Keyboard {
+  constructor() {
+
+  }
+}
+
+const keydata = {
+  {
+    layer: 0,
+    name: 'qwerty',
+    chars: {
+      {
+        char: 'q',
+        width: 5,
+        height: 7,
+        l1: '-####',
+        l2: '#---#',
+        l3: '#---#',
+        l4: '-####',
+        l5: '----#',
+        l6: '----#',
+        l7: '----#',
+      }
+    }
+  }
+}
