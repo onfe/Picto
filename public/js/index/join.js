bfc1eef4dee26288684df8405d95fff254387312
Edward H <eddy.hey@outlook.com>
2018-10-08 17:20:07 +0100

    Add redirect on room unavailable.

--- public/js/index/join.js
+++ public/js/index/join.js
@@ -4,32 +4,35 @@ var roomCode = path[path.length - 1]
 
 $(function () {
 
-  $("#room-id").html(roomCode);
+  $('#room-id').html(roomCode);
 
   $.ajax({
-    url: "http://" + window.location.host + "/api/room/",
+    url: 'http://' + window.location.host + '/api/room/',
     data: {room: roomCode},
   }).done( room );
 
-  $("#join").on('click', function() {
+  $('#join').on('click', function() {
     // Get code from input box
-    var name = $("#username").val();
+    var name = $('#username').val();
 
     // Perform server side initial validation
     $.ajax({
-      url: "http://" + window.location.host + "/api/username/",
+      url: 'http://' + window.location.host + '/api/username/',
       data: {room: roomCode, name: name},
     }).done( validateName );
   });
 });
 
 function room(e) {
+  if (!e.available) {
+    location.href = '/';
+  }
   $('#room-clients').html(e.numCli)
 }
 
 function validateName(e) {
   if (e.available) {
-    Cookies.set("user", e.name)
-    location.href = "/room/" + roomCode + "/";
+    Cookies.set('user', e.name)
+    location.href = '/room/' + roomCode + '/';
   };
 };
