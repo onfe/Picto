80fd0c329870a48cff6741a3d2c3a772a25949be
Edward <onfe@users.noreply.github.com>
2019-10-15 22:47:16 +0100

    Archive Picto into Picto/old.

--- public/js/chat/sound.js
+++
@@ -1,7 +0,0 @@
-module.exports = class Sound {
-  constructor() {
-    this.join = new Audio('/snd/bell.mp3');
-    this.button = new Audio('/snd/click.mp3');
-    this.message = new Audio('/snd/message.mp3');
-  }
-}
--- public/js/chat/stamp.js
+++
@@ -1,8 +0,0 @@
-module.exports = class Stamp {
-  constructor(char, width, height, data) {
-    this.char = char;
-    this.width = width;
-    this.height = height;
-    this.data = data;
-  }
-}
