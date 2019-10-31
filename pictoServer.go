c153c9eda3213fa1e98a13617b13c8e438d85854
Joshua O'sullivan <joinks1999@gmail.com>
2019-10-31 01:57:24 +0000

    Add API token & improve API to respond with default message if method is unrecognised.

--- pictoServer.go
+++ pictoServer.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 	"os"
@@ -13,15 +14,25 @@ var roomManager server.RoomManager
 func main() {
 	roomManager = server.NewRoomManager(server.MaxRooms)
 
-	fs := http.FileServer(http.Dir("../client/dist"))
+	fs := http.FileServer(http.Dir("client/dist"))
 	http.Handle("/", fs)
 
 	http.HandleFunc("/ws", roomManager.ServeWs)
-	http.HandleFunc("/api/", roomManager.ServeAPI)
 
 	address := ":8080"
 	if os.Getenv("GO_ENV") == "PRODUCTION" {
 		address = ":80"
+		if _, exists := os.LookupEnv("API_TOKEN"); exists {
+			http.HandleFunc("/api/", roomManager.ServeAPI)
+		} else {
+			http.HandleFunc("/api/", func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Set("Content-Type", "application/json")
+				response, _ := json.Marshal("This feature has been disabled.")
+				w.Write(response)
+			})
+		}
+	} else {
+		http.HandleFunc("/api/", roomManager.ServeAPI)
 	}
 
 	log.Fatal(http.ListenAndServe(address, nil))
