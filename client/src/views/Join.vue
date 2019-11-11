7079ffa76d29db6eb3c2d4d5d8f81c8783b9dafd
Edward <onfe@users.noreply.github.com>
2019-11-11 20:50:58 +0000

    Add join system.

---
+++ client/src/views/Join.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <form @submit.prevent="join">
+      <input v-model="name" name="name" />
+    </form>
+    <button @click="join" type="button" name="button">Join</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  components: {},
+  data() {
+    return {
+      name: ""
+    };
+  },
+  created() {
+    console.log(this.$route);
+  },
+  methods: {
+    join() {
+      const room = this.$route.params.id;
+      const name = this.name;
+      if (room) {
+        this.$store.dispatch("client/join", { name, room });
+      } else {
+        this.$store.dispatch("client/join", { name });
+      }
+    }
+  }
+};
+</script>
