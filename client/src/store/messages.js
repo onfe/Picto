8bfa168be4d2b576a211a68c5d917b32fdf01d0f
Edward <onfe@users.noreply.github.com>
2019-11-12 15:44:52 +0000

    Add message store.

---
+++ client/src/store/messages.js
@@ -0,0 +1,22 @@
+const state = {
+  history: [
+    {name: 'jon', data: 'ahsdald', colour: '#ffcced'},
+    {name: 'jon', data: 'ahsdald', colour: '#ffcced'}
+  ]
+};
+
+const getters = {};
+
+const actions = {};
+
+const mutations = {};
+
+const client = {
+  namespaced: true,
+  state: state,
+  getters: getters,
+  actions: actions,
+  mutations: mutations
+};
+
+export default client;
