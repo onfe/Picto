e95dbae2aeeaa195c49776677a3293abad748479
Edward <onfe@users.noreply.github.com>
2019-11-21 00:48:12 +0000

    Add compose store.

---
+++ client/src/store/compose.js
@@ -0,0 +1,30 @@
+const state = {};
+
+const getters = {};
+
+const actions = {
+  send: ({ dispatch }) => {
+    const data = window._sketch.getSendableData();
+    console.log({data});
+    const pl = {
+      event: "message",
+      data: data
+    }
+    dispatch("socket/send", pl, { root: true });
+  },
+  clear: () => {
+    window._sketch.clear();
+  }
+};
+
+const mutations = {};
+
+const client = {
+  namespaced: true,
+  state: state,
+  getters: getters,
+  actions: actions,
+  mutations: mutations
+};
+
+export default client;
