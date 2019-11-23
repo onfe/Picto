26068045092ed1bd6a619e3be2c6516424b3bfc7
Edward <onfe@users.noreply.github.com>
2019-11-23 01:17:20 +0000

    Separate message and announcement.

---
+++ client/src/components/CanvasMessage.vue
@@ -0,0 +1,60 @@
+<template lang="html">
+  <Message :author="author" :colour="colour">
+    <canvas v-bind:id="idHash"></canvas>
+  </Message>
+</template>
+
+<script>
+import Notepad from "@/assets/js/notepad.js";
+import Message from "@/components/Message.vue";
+
+export default {
+  components: {
+    Message
+  },
+  props: {
+    data: {
+      type: Object,
+      default: () => {
+        return {
+          span: 192,
+          data: Array(192 * 64).fill(0)
+        }
+      }
+    },
+    author: {
+      type: String,
+      default: "Author"
+    },
+    colour: {
+      type: String,
+      default: "#e97777"
+    }
+  },
+  data() {
+    return {
+      idHash: null
+    }
+  },
+  beforeMount() {
+    this.idHash = Math.random().toString(36).substring(2, 15)
+  },
+  mounted() {
+    const canv = document.getElementById(this.idHash);
+    this.notepad = new Notepad(192, 64, canv);
+    this.notepad.loadImageData(this.data);
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+canvas {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  image-rendering: pixelated;
+  touch-action: none;
+}
+</style>
