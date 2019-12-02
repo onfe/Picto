f5c72c3648840c680b82669ab4634e7d44a74647
Edward <onfe@users.noreply.github.com>
2019-12-02 23:42:02 +0000

    Add keyboard support.

--- client/src/plugins/fontawesome-vue.js
+++ client/src/plugins/fontawesome-vue.js
@@ -4,12 +4,13 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import {
   faPencilAlt,
   faEraser,
-  faCircle
+  faCircle,
+  faKeyboard
 } from "@fortawesome/free-solid-svg-icons";
 import { faDotCircle } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 // Add the required icons to keep the bundle small.
-library.add(faPencilAlt, faEraser, faCircle, faDotCircle);
+library.add(faPencilAlt, faEraser, faCircle, faDotCircle, faKeyboard);
 
 Vue.component("font-awesome-icon", FontAwesomeIcon);
