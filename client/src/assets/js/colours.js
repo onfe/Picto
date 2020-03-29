a52e9bfb3c66eb85158416a627c67759302abf1c
Joshua O'Sullivan <jrosullivan1@sheffield.ac.uk>
2020-03-29 21:18:54 +0100

    Random colours are forbidden

--- client/src/assets/js/colours.js
+++ client/src/assets/js/colours.js
@@ -14,36 +14,15 @@ const COLOURS = [
 function colour(n) {
   /*
     Returns a hex colour that will always be the same for a given n.
-    The first 8 colours are set to ensure they look good.
+    The first 3 colours are set for branding purposes.
     n is zero indexed.
   */
-  if (n < 8) {
+  if (n < 3) {
     return COLOURS[n];
   } else {
-    var hue = sprng(7930832, n - 8) * 360
-    return Color.hsl([hue, 90, 65]).hex();
+    var hue = Math.floor(((360 / (Math.E / 2)) * n) % 360);
+    return Color.hsl([hue, 72, 69]).hex();
   }
 }
-
-function sprng(seed, n) {
-  /* calculate the nth seeded float between 0-1.
-   * both seed and n must be an integer.
-   * Runs in O(n)
-   */
-
-  // normalise n >= 1
-  n = Math.abs(n) + 1;
-
-  // normalise the seed
-  seed = seed % 2147483647;
-  seed = seed <= 0 ? seed + 2147483646 : seed;
-
-  for (var i = 0; i <= n; i++) {
-    seed = seed * 16807 % 2147483647;
-  }
-  return Math.abs(seed - 1) / 2147483646;
-}
-
-
 
 export default colour;
