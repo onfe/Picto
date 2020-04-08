f6915ca4dd8532e7ade497816ad289e885487453
Joshua O'Sullivan <jrosullivan1@sheffield.ac.uk>
2020-04-08 19:04:28 +0100

    refactor pictoServer.go

--- pictoServer.go
+++ pictoServer.go
@@ -1,66 +1,17 @@
 package main
 
 import (
-	"bytes"
 	"io"
-	"io/ioutil"
 	"log"
 	"math/rand"
 	"net/http"
 	"os"
 	"strconv"
-	"strings"
-	"time"
 
 	"github.com/onfe/Picto/src/server"
 )
 
 var roomManager server.RoomManager
-
-//CustomResponseWriter ...
-type CustomResponseWriter struct {
-	http.ResponseWriter
-	status int
-}
-
-//WriteHeader ...
-func (w *CustomResponseWriter) WriteHeader(status int) {
-	w.status = status
-	if status != http.StatusNotFound {
-		w.ResponseWriter.WriteHeader(status)
-	}
-}
-
-func (w *CustomResponseWriter) Write(data []byte) (int, error) {
-	if w.status != http.StatusNotFound {
-		return w.ResponseWriter.Write(data)
-	}
-	return len(data), nil
-}
-
-func getFileHandler(h http.Handler) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		customResponseWriter := &CustomResponseWriter{ResponseWriter: w}
-
-		h.ServeHTTP(customResponseWriter, r)
-
-		if customResponseWriter.status == 404 {
-			log.Println("[REDIRECT] - To index.html, from:", r.RequestURI)
-			data, _ := ioutil.ReadFile("dist/index.html")
-			w.Header().Set("Content-Type", "text/html")
-			http.ServeContent(w, r, "index.html", time.Now(), bytes.NewReader(data))
-		}
-	}
-}
-
-func loadWordsList(fp string) []string {
-	data, err := ioutil.ReadFile(fp)
-	if err != nil {
-		log.Println("[SYSTEM] - Couldn't open words list.")
-		panic(err)
-	}
-	return strings.Split(strings.Replace(string(data), "\r\n", "\n", -1), "\n")
-}
 
 func main() {
 	//Logfile setup
@@ -76,7 +27,7 @@ func main() {
 	go NewMonitor(60)
 
 	//Loading words list
-	wordsList := loadWordsList("words.txt")
+	wordsList := server.LoadWordsList("words.txt")
 
 	//Getting env var values
 	apiToken, tokenSet := os.LookupEnv("API_TOKEN")
